c400a51dfde0f5df7f05cfaeb8838924938bd011
L <41794465+LLL15@users.noreply.github.com>
2018-08-04 16:01:03 +0200

    Fix regular expression bug

--- Main.py
+++ Main.py
@@ -3,12 +3,12 @@
 import os
 import re
 
-ipaddr = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+ipaddr = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(/\d{1,2})?$')
 
 
 def scan(ip, args=''):
     while ipaddr.search(ip) is None:
-        ip = input('Please enter correct IP|IP Range: ')
+        ip = input('Please enter correct IP: ')
         if ipaddr.search(ip) is not None:
             break
 
@@ -28,20 +28,19 @@ def view_help(userinp):
         print(str(process.read()))
         quit_or_continue = input("Start scan now [yes|no]: ").lower()
         if quit_or_continue == 'yes':
-            print('\n')
             start_scan()
         else:
             print("Exiting the script . . .")
             raise SystemExit
     elif userinp == 'no':
-        print('\n')
         start_scan()
 
 
 def start_scan():
     while 1:
+        print(' ')
         print("Performing a scan...")
-        print(scan(input("Enter IP address or Range: "), input("Enter arguments (enter for none):")))
+        print(scan(input("Enter IP address: "), input("Enter arguments (enter for none):")))
         scan_again = input("Do you want to keep scanning [yes|no]: ").lower()
         if scan_again == 'yes':
             continue
